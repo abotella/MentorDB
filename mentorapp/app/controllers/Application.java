51f3d88dcc3d85092570b3e5b09d24c3e177c513
ry1015 <rryy19@hotmail.com>
2014-04-10 12:17:52 -0700

    Testing agian

--- mentorapp/app/controllers/Application.java
+++ mentorapp/app/controllers/Application.java
@@ -8,9 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-    	//CHecking if this is working
-    	//LETS GET IT ON!!!
-        return ok(index.render("Your new application is ready. Is it really ready??"));
+    	return ok(index.render("Your new application is ready. Is it really ready??"));
     }
 
 }
