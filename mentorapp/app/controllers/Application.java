adc4e07a8ab0f4afdc19c742c81344a47833a303
ry1015 <rryy19@hotmail.com>
2014-04-10 12:18:57 -0700

    I guess it is working now.

--- mentorapp/app/controllers/Application.java
+++ mentorapp/app/controllers/Application.java
@@ -8,6 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+    	String test = "Using a new computer";
     	return ok(index.render("Your new application is ready. Is it really ready??"));
     }
 
