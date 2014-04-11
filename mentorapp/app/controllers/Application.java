80e808d6023a8e2b0a21704db111d8d369a6475b
Charlemagne <illestsf@live.com>
2014-04-10 19:56:40 -0700

    Testing
    Added template

--- mentorapp/app/controllers/Application.java
+++ mentorapp/app/controllers/Application.java
@@ -1,15 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.data.Form;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Page1;
 
 public class Application extends Controller {
 
+	/**
+     * Returns the home page. 
+     * @return The resulting home page. 
+     */
     public static Result index() {
-    	String test = "Using a new computer";
-    	return ok(index.render("Your new application is ready. Is it really ready??"));
+      return ok(Index.render("Testing: This is the homepage."));
     }
-
+    
+    /**
+     * Returns page1.
+     * @return The Page1.
+     */
+    public static Result page1() {
+      return ok(Page1.render("Welcome to Page 1."));
+    }
+    
 }
