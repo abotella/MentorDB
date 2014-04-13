0186cbbe3c3dc41ed8562a35c4f821960b378845
Michael Daniels <michael.edaniels47@gmail.com>
2014-04-13 11:30:54 -0700

    Bug fixes
    Semicolons and stuff

--- mentorapp/app/controllers/AdministratorFunctions.java
+++ mentorapp/app/controllers/AdministratorFunctions.java
@@ -2,31 +2,62 @@ package controllers;
 
 public class AdministratorFunctions
 {
+	/**
+	 * Validates the user's login credentials
+	 * @param userN the username that the user entered
+	 * @param pass the password that the user entered
+	 * @retun whether or not the user entered valid information
+	 */
 	public boolean validate(String userN, String pass)
 	{
-		return false
+		return false;
 	}
 	
+	/**
+	 * Responds to a message that was sent to the administrator by a user
+	 * @param message the message that will be sent to the user
+	 */
 	public void respond(Message message)
 	{
 		
 	}
 	
+	/**
+	 * Searches for a user profile based on a username
+	 * @param username the username that will be used to search for a User profile
+	 * @return the User that is attached to the username
+	 */
 	public User searchUser(String username)
 	{
 		return null;
 	}
 	
+	/**
+	 * Access a User's profile to verify information about the user
+	 * @param user the User that the Administrator wishes to access
+	 */
 	public void accessUserProfile(User user)
 	{
 		
 	}
 	
+	/**
+	 * Resets a User's password to a default password based on username
+	 * @param username the User to reset the password for
+	 * @return whether or not the password was successfully changed
+	 */
 	public boolean resetUserPassword(String username)
 	{
 		return false;
 	}
 	
+	/**
+	 * Resets a User's password to a password given to the Administrator by the User
+	 * @param username the User to reset the password for
+	 * @param newPassword the password given to the Administrator by the User that will be the
+	 * User's new passowrd
+	 * @return whether or not the password was successfully changed
+	 */
 	public boolean resetUserPassword(String username, String newPassword)
 	{
 		return false;
--- mentorapp/app/models/Administrator.java
+++ mentorapp/app/models/Administrator.java
@@ -4,25 +4,43 @@ public class Administrator
 {
 	String username, password;
 	
+	/**
+	 * Creates a new Administrator
+	 * @param userN the chosen username for the administrator
+	 * @param pass the chosen password the administrator
+	 */
 	public Administrator(String userN, String pass)
 	{
 		username = userN;
 		password = pass;
 	}
 	
+	/**
+	 * Sets a new username for the administrator
+	 * @param newUserName the new username that Administrator will be changed to
+	 */
 	public void setUsername(String newUserName)
 	{
 		username = newUserName;
 	}
 	
+	/**
+	 * Sets a new password for the Administrator
+	 * @param newPassword the new password that Administrator will have
+	 */
 	public void setPassword(String newPassword)
 	{
 		password = newPassword;
 	}
 	
+	/**
+	 * Verifies the Administrator's password for login
+	 * @param pass the password the user entered that will be compared to Administrator's password
+	 * @return whether or not the two passwords match
+	 */
 	public boolean verifyPassword(String pass)
 	{
-		return pass == boolean;
+		return pass == password;
 	}
 	
 }
