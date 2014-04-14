7b249a1b7525b1ad8d59b6243d63c63a583248c6
jonathan510 <xxl360@gmail.com>
2014-04-13 17:07:06 -0700

    new

---
+++ mentorapp/app/controllers/Users.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import java.util.Set;
+
+import models.User;
+//import play.api.data.*;
+import play.data.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+public class Users extends Controller {
+	private static final Form<User> userForm = Form.form(User.class);
+	
+	public static Result list() {
+		Set<User> users = User.findAll();
+		return ok(list.render(users));
+	}
+
+	
+	public static Result save() {
+		return ok();
+	}
+
+	public static Result show(Integer ean) {
+		return ok();
+	}
+
+	public static Result showBlank() {
+		return ok(show.render(userForm));
+	}
+}
