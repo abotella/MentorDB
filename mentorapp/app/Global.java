cb78ed1cf1e24a4be7f9af3e4c24c2fe6f390848
jonathan510 <xxl360@gmail.com>
2014-04-18 19:34:03 -0700

    updated userSetup to send variables . commented out global

---
+++ mentorapp/app/Global.java
@@ -0,0 +1,28 @@
+/**import models.UserDB;
+import play.Application;
+import play.GlobalSettings;
+
+/**
+ * Initialization code for the application.
+ * 
+ */
+/**public class Global extends GlobalSettings {
+
+  	/**
+	 * On start up, initialize a mock database with sample contacts (name,
+	 * username, password).
+	 * 
+	 * 
+	 * @param app
+	 *            The application.
+	 *
+  public void onStart(Application app) {
+    UserDB.addUserInfo("Charlemagne Doles", "Charlemagne@sjsu.com", "password");
+    UserDB.addUserInfo("Ryan Honrado", "Ryan@sjsu.com", "password");
+    UserDB.addUserInfo("Elisa Yan", "Elisa@sjsu.com", "password");
+    UserDB.addUserInfo("Jonathan Cruz", "Jonathan@sjsu.com", "password");
+    UserDB.addUserInfo("Aymeric Botella", "Aymeric@sjsu.com", "password");
+    UserDB.addUserInfo("Michael Daniels", "Michael@sjsu.com", "password");
+		UserDB.addUserInfo("Admin", "Admin", "tech");
+  }
+}*/
